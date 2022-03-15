9272ba6f5530a310479bacaf7c25503b1507ae31
LloydDub <lloydrww@gmail.com>
2022-03-14 19:30:59 -0600

    save layer function added

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const { createCanvas, loadImage } = require("canvas");
 const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext("2d");
 
+//This saves the newly created image
+const saveLayer = (_canvas) => {
+  fs.writeFileSync("./newImage.png", _canvas.toBuffer("image/png"));
+  console.log("🎨Image Created🖌️");
+};
+
+// This draws the image based on ctx settings in function
 const drawLayer = async () => {
   const image = await loadImage("./shiitake.png");
-  console.log(data);
+  ctx.drawImage(image, 0, 0, 500, 500);
+  console.log("Program Ran 🧑‍🎨");
+  saveLayer(canvas);
 };
 
 drawLayer();
